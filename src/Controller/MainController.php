84ec310dd11250f9e6732d576bfdb97109aae6bd
adisonzenemij <adisonzenemij@gmail.com>
2024-11-23 22:16:37 -0500

    feat: developed project

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,6 @@
     namespace App\Controller;
 
     use App\Core\Render;
-    use App\Library\Identified;
 
     class MainController {
         final public function __construct() {}
@@ -14,24 +13,6 @@
             $templates = ['main'];
             # Renderiar plantilla y configuracion
             Render::view($templates, $data);
-            # Generar UUID oficial
-            $this->trigger();
-            # Generar UUID personalizado
-            $this->short();
-        }
-
-        public function trigger() {
-            # Generar UUID oficial
-            $uuidTrigger = Identified::trigger() . "<br/>";
-            # Imprimir el UUID generado
-            echo "UUID:" . " " . $uuidTrigger;
-        }
-
-        public function short() {
-            # Generar UUID personalizado
-            $uuidShort = Identified::short(25, 5);
-            # Imprimir el UUID generado
-            echo "UUID:" . " " . $uuidShort . "<br/>";
         }
     }
 ?>
